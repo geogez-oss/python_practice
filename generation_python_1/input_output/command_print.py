db5f6fba60f0d135ef73b4a94a55b2fd3a4162f7
George Gesalyanz <geogez@MacBook-Pro-George.local>
2026-03-16 22:35:28 +0300

    upload

--- generation_python_1/input_output/command_print.py
+++ generation_python_1/input_output/command_print.py
@@ -1,1 +1,23 @@
-print('Здравствуй, мир!')
+# print('Здравствуй, мир!')
+# print(4, 8, 15, 16, 23, 42)
+# print(4, 8, 15, 16, 23, 42,sep='\n')
+#
+# for i in range(1, 8):
+#     print(i * '*')
+#
+# name = input()
+# print('Привет,', name)
+#
+# fk = input()
+# print(fk, '- чемпион')
+
+
+str_1 = input()
+str_2 = input()
+str_3 = input()
+print(str_1, str_2, str_3, sep='\n')
+
+str_1 = input()
+str_2 = input()
+str_3 = input()
+print(str_3, str_2, str_1, sep='\n')
---
+++ generation_python_1/input_output/sep_end.py
@@ -0,0 +1,9 @@
+print('I', 'like', 'Python', sep='***')
+
+print('Привет', input(), sep=', ', end='!')
+
+separator = input()
+str_1 = input()
+str_2 = input()
+str_3 = input()
+print(str_1, str_2, str_3, sep=separator, end='\n')
