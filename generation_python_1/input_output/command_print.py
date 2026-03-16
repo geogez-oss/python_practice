230edaa6b1592f6528438a7ddbb0f9f13d9abd4c
George Gesalyanz <geogez@MacBook-Pro-George.local>
2026-03-16 22:41:01 +0300

    some changes

--- generation_python_1/input_output/command_print.py
+++ generation_python_1/input_output/command_print.py
@@ -1,15 +1,15 @@
-# print('Здравствуй, мир!')
-# print(4, 8, 15, 16, 23, 42)
-# print(4, 8, 15, 16, 23, 42,sep='\n')
-#
-# for i in range(1, 8):
-#     print(i * '*')
-#
-# name = input()
-# print('Привет,', name)
-#
-# fk = input()
-# print(fk, '- чемпион')
+print('Здравствуй, мир!')
+print(4, 8, 15, 16, 23, 42)
+print(4, 8, 15, 16, 23, 42,sep='\n')
+
+for i in range(1, 8):
+    print(i * '*')
+
+name = input()
+print('Привет,', name)
+
+fk = input()
+print(fk, '- чемпион')
 
 
 str_1 = input()
