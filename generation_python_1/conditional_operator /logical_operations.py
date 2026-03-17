d6ef100609d31f9333c45fd75ed2aaccb7160bab
George Gesalyanz <geogez@MacBook-Pro-George.local>
2026-03-17 22:23:17 +0300

    upload

---
+++ generation_python_1/conditional_operator /logical_operations.py
@@ -0,0 +1,11 @@
+number = int(input())
+if -1 < number and number < 17:
+    print('Принадлежит')
+else:
+    print('Не принадлежит')
+
+number = int(input())
+if number <= -3 or number >= 7:
+    print('Принадлежит')
+else:
+    print('Не принадлежит')
